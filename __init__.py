0ff49cda1977f6b04c87973fdc7698aa97ccd034
juniorfarias <junior@unhideschool.com>
2020-01-23 15:29:36 -0300

    Updated UHQL_SqlAlchemy default implamentation

--- __init__.py
+++ __init__.py
@@ -10,8 +10,8 @@ from .main.basetypes import UHQLException, UHQLUserRequest
 
 # Default Implementation
 def UHQL_SqlAlchemy(
-    model_base: DeclarativeMeta, dbsession=None, extra_type_injector=None, can_func=None
+    model_base: DeclarativeMeta, dbsession=None, extra_type_injector=None, can_func=None, post_func=None
 ) -> UHQL:
     return UHQL(
-        UHQLSqlAlchemyDataProvider(model_base, dbsession), extra_type_injector, can_func
+        UHQLSqlAlchemyDataProvider(model_base, dbsession), extra_type_injector, can_func, post_func
     )
